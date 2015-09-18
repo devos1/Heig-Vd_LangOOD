a8fba70bf8f77418109f2b7253180eb916554bcd
Devos <nicky.larson.one@hotmail.com>
2015-09-18 07:25:35 +0200

    Start coding

--- LangOOD.Exercices/CH01.Exercice01/Program.cs
+++ LangOOD.Exercices/CH01.Exercice01/Program.cs
@@ -8,6 +8,24 @@ namespace CH01.Exercice01
 {
     class Program
     {
+        static Boolean traitement(int nbIteration, float valeurDepart, float valeurFinale)
+        {
+            try
+            {
+                for (int i = 0; i < nbIteration; i++)
+                {
+                    valeurFinale += (i * 2);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return false;
+        }
+
         static void Main(string[] args)
         {
         }

c511bf19a48ec1d7857131b2f3de461b18c8ffd2
Oscar <devos1@bluewin.ch>
2015-09-20 13:22:30 +0200

    Finished first version of code

--- LangOOD.Exercices/CH01.Exercice01/Program.cs
+++ LangOOD.Exercices/CH01.Exercice01/Program.cs
@@ -8,26 +8,63 @@ namespace CH01.Exercice01
 {
     class Program
     {
-        static Boolean traitement(int nbIteration, float valeurDepart, float valeurFinale)
+        static int nbIteration;
+        static float valeurDepart;
+        static float valeurFinale;
+
+        static Boolean traitement(int nbIter, float vd, float vf)
         {
             try
             {
-                for (int i = 0; i < nbIteration; i++)
+                vf = vd;
+                for (int i = Convert.ToInt32(vd); i < nbIter; i++)
                 {
-                    valeurFinale += (i * 2);
+                    vf += ((i-1) * 2);
                 }
             }
-            catch (Exception)
+            catch (Exception e)
             {
 
-                throw;
+                Console.WriteLine("Erreur : " + e.Message);
+                return false;
             }
 
-            return false;
+            if (vf.GetType() == typeof(System.Single))
+            {
+                valeurFinale = vf;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Erreur : La varaible n'est pas un float!");
+                return false;
+            }
         }
 
         static void Main(string[] args)
         {
+            string line;
+
+            Console.Write("Entre nombre d'itérations : ");
+            line = Console.ReadLine();
+            nbIteration =  Int32.Parse(line);
+
+            Console.Write("Entre numéro de départ : ");
+            line = Console.ReadLine();
+            valeurDepart = float.Parse(line);
+
+            Console.Write("Entre numéro de fin : ");
+            line = Console.ReadLine();
+            valeurFinale = float.Parse(line);
+
+            if (traitement(nbIteration, valeurDepart, valeurFinale))
+            {
+                Console.WriteLine("Le traitment s'est bien passé.");
+                Console.WriteLine("Valeur finale = {0}", valeurFinale);
+            }
+
+            Console.ReadKey();
+
         }
     }
 }

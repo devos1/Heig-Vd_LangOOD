ae4df3510e8c3edf901dbdcc549b68b2499763b5
Devos <nicky.larson.one@hotmail.com>
2015-09-26 14:57:27 +0200

    Ajout exercice Echiquier GUI

---
+++ LangOOD.Exercices/CH10.EchiquierGUI/Echiquier.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CH10.EchiquierGUI
+{
+    enum Pieces { roi, reine, dame, fou, cavalier, tour, pion }
+    struct Point
+    {
+        private int x, y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX()
+        {
+            return this.x;
+        }
+    
+        public int getY()
+        {
+            return this.y;
+        }
+    }
+
+    class Echiquier
+    {
+        private Pieces[,] echiquier;
+
+        public  Echiquier()
+        {
+            // Définition de la grille
+            echiquier = new Pieces[8, 8];
+            echiquier[0, 0] = Pieces.tour;
+        }
+
+        public List<Pieces> getPiecesLigne(int noLigne)
+        {
+            List<Pieces> listeP = new List<Pieces>();
+
+            for (int i = 0; i < 7; i++)
+            {
+                listeP.Add(echiquier[noLigne, i]);                
+            }
+
+            return listeP;
+        }
+
+
+        public Pieces getPiecePosition(int x, int y)
+        {
+            Pieces piece = echiquier[x, y];
+            return piece;
+        }
+
+
+        public Point? PlacerPiece(int x, int y, Pieces piece)
+        {
+            if (x < 0 || x > 7 || y < 0 || y > 7)
+            {
+                MessageBox.Show("Erreur, les positions x et y doivent être comprises entre 0 et 7");
+                return null;
+            }
+            else
+            {
+                Point p = new Point(x, y);
+                return p;
+            }
+        }
+    }
+}
+
---
+++ LangOOD.Exercices/CH10.EchiquierGUI/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CH10.EchiquierGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Création Labels pour les pièces dans la grille
+            Label lblA1 = new Label();
+            Label lblA2 = new Label();
+
+
+            Echiquier echiquier = new Echiquier();
+            Pieces p = echiquier.getPiecePosition(0, 0);
+
+            List<Pieces> listeP = new List<Pieces>();
+            listeP = echiquier.getPiecesLigne(0);
+
+            // Affichage ligne 1
+
+
+            //essageBox.Show(p.ToString());
+
+            lblA1.Content = p.ToString();
+            lblA2.Content = "test";
+            a1.Children.Add(lblA1);
+            a2.Children.Add(lblA2);
+
+        }
+
+        private void createGrid()
+        {
+            // Create the Grid
+            Grid DynamicGrid = new Grid();
+            DynamicGrid.Width = 400;
+            DynamicGrid.HorizontalAlignment = HorizontalAlignment.Left;
+            DynamicGrid.VerticalAlignment = VerticalAlignment.Top;
+            DynamicGrid.ShowGridLines = true;
+            DynamicGrid.Background = new SolidColorBrush(Colors.LightSteelBlue);
+        }
+    }
+}

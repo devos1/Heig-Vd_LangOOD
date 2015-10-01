57a130da9528b0e63150b155ac5e5115859128a7
Devos <nicky.larson.one@hotmail.com>
2015-10-01 21:28:44 +0200

    Ajout exercice divers sur Interfaces

---
+++ LangOOD.Exercices/Misc.01.Interfaces/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misc._01.Interfaces
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VehiculeAmphibie v = new VehiculeAmphibie();
+
+            Ivoiture iv = v;
+            Iamphibie ia = v;
+
+            iv.bouger();
+            ia.bouger();
+        }
+    }
+}
---
+++ LangOOD.Exercices/Misc.01.Interfaces/VehiculeAmphibie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misc._01.Interfaces
+{
+    interface Iamphibie
+    {
+        void bouger();
+    }
+
+    interface Ivoiture
+    {
+        void bouger();
+    }
+
+    class VehiculeAmphibie : Iamphibie, Ivoiture
+    {
+        void Ivoiture.bouger()
+        {
+            Console.WriteLine("Je bouge en tant que voiture!!");
+        }
+
+        void Iamphibie.bouger()
+        {
+            Console.WriteLine("Je bouge en tant que amphibie!!");
+        }
+    }
+}

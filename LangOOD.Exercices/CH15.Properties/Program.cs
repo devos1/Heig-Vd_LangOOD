bf62f6dbd1f02279b182ff8f4411f85df2854f90
Oscar <devos1@bluewin.ch>
2015-10-10 16:30:48 +0200

    Ajout exercice ch.15 sur les propriétés

---
+++ LangOOD.Exercices/CH15.Properties/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH15.Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Voiture v1 = new Voiture();
+            Console.WriteLine("La vitesse actuelle est de {0} km/h", v1.VitesseCourante);
+            v1.Accelerer(10.2);
+            Console.WriteLine("La vitesse actuelle est de {0} km/h", v1.VitesseCourante);
+            v1.Accelerer(30);
+            Console.WriteLine("La vitesse actuelle est de {0} km/h", v1.VitesseCourante);
+        }
+    }
+}
---
+++ LangOOD.Exercices/CH15.Properties/Voiture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH15.Properties
+{
+    class Voiture
+    {
+        public double VitesseCourante { get; private set; }
+
+        public Voiture()
+        {
+            VitesseCourante = 0;
+            Console.WriteLine("Une voiture vient d'être créée.");
+        }
+
+        public void Accelerer(double a)
+        {
+            VitesseCourante += a;
+        }
+    }
+}
